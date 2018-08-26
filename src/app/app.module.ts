9f8c4c7ebcab6ce6d4460c76ebeebba5ea2752b5
Максим маркин <maestral4ik@mail.ru>
2018-08-26 20:18:24 +0300

    registration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms"
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -9,6 +10,11 @@ import { LoginPageComponent } from './components/login-page/login-page.component
 import { PrivatePageComponent } from './components/private-page/private-page.component';
 import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
 import { AppRoutingModule } from './app-routing.module';
+
+import { AuthService } from "./services/auth.service";
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { environment } from './../environments/environment';
 
 @NgModule({
   declarations: [
@@ -22,9 +28,12 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(environment.fireBaseConfig)
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/register-page/register-page.component.ts
+++ src/app/components/register-page/register-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthService } from "../../services/auth.service";
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-register-page',
   templateUrl: './register-page.component.html',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPageComponent implements OnInit {
 
-  constructor() { }
+  public email: string;
+  public password: string;
+
+  constructor(public auth: AuthService, public router: Router) { }
 
   ngOnInit() {
   }
 
+  onSubmtiAddUser(){
+    this.auth.registration(this.email, this.password)
+    .then( (res) => {
+      this.router.navigate(['/private']);
+    }).catch((err) => {
+      console.log(err)
+    })
+  }
 }
